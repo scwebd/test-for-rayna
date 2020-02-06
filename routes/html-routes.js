34ff24631a7a65b98af865e652b7d580ca0bb4dc
rayna williams <Raynawilliams123@gmail.com>
2020-02-06 14:16:33 -0700

    got the signup for passport working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,22 +4,26 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/test", function(req, res) {
-    res.render("index" , {message:"hi" });
+  app.get("/test", function (req, res) {
+    res.render("index", { message: "hi" });
   });
 
   // cms route loads cms.html
-  app.get("/cms", function(req, res) {
+  app.get("/cms", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/layouts/cms.html"));
   });
-  app.get("/login", function(req, res) {
-    res.render("login", {message: "yo"});
-  })
+  app.get("/login", function (req, res) {
+    res.render("login", { message: "yo" });
+  });
+  app.get("/contact", function (rea, res) {
+    res.render("contact", { message: "hi" });
+  });
+
   // blog route loads blog.html
   // app.get("/handy", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/handy.html"));
